974811604c9559941befde037575c0d8a0842fe8
Arsalan Rashid <arsalanrashidthoker@gmail.com>
2024-02-23 19:38:45 +0530

    test hook

--- main.go
+++ main.go
@@ -13,9 +13,6 @@ func main() {
 	fmt.Println("bullshit programmer...")
 	fmt.Println("lcnt auth running")
 	fmt.Println("lcnt auth running")
-	fmt.Println("lcnt auth running")
-	fmt.Println("lcnt auth running")
-	fmt.Println("lcnt auth running")
 	r := gin.Default()
 	conf := &oauth2.Config{
 		ClientID:     "e52d7c7e965a974ac487",
